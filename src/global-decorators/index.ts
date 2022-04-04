40ceff79d99d4ddefa32bc5548efdbd86d4a64d8
Martin Šepeľa <xsepela@fi.muni.cz>
2022-04-04 10:46:41 +0200

    feat: Added new utilities
    Basic parser for the Id in the route Params + folder for global constants

---
+++ src/global-decorators/index.ts
@@ -0,0 +1,1 @@
+export * from './params-id.decorator';
--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get<ConfigService>(ConfigService);
 
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
 
   app.use(
     session({
