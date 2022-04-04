f0815ab7b9cfb76a94917f65dbda28d79630c386
Martin Šepeľa <xsepela@fi.muni.cz>
2022-04-04 20:21:10 +0200

    fix: Rewrote user service into exceptions so I dont query database twice for existance

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,5 +1,6 @@
-import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
+import { BadRequestException, ForbiddenException, Injectable, NotFoundException } from '@nestjs/common';
 import { prisma, UserRole } from '@prisma/client';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 import * as argon from "argon2";
 import { instanceToInstance, plainToClass } from 'class-transformer';
 import { PrismaService } from '../prisma/prisma.service';
@@ -10,7 +11,15 @@ export class UserService {
     constructor(private prismaService: PrismaService) { }
 
     public async getUserDto(userId: number) {
-        const user = await this.checkExistsOrThrowException(userId);
+        const user = this.prismaService.user.findFirst({
+            where: {
+                id: userId
+            }
+        });
+
+        if (!user) {
+            return new NotFoundException('User not found');
+        }
 
         return plainToClass(UserDto, { ...user, password: "" }, { excludeExtraneousValues: true });
     }
@@ -21,77 +30,70 @@ export class UserService {
     }
 
     public async updateUserHimself(userToUpdateId: number, updateDataDto: UserDto) {
-        await this.checkExistsOrThrowException(userToUpdateId);
+        try {
+            const hash = await argon.hash(updateDataDto.password);
 
-        const hash = await argon.hash(updateDataDto.password);
+            await this.prismaService.user.update({
+                where: {
+                    id: userToUpdateId
+                },
+                data: {
+                    firstName: updateDataDto.firstName,
+                    lastName: updateDataDto.lastName,
+                    email: updateDataDto.email,
+                    passwdHash: hash,
+                }
+            });
 
-        updateDataDto.roles = Array.from(new Set(updateDataDto.roles));
-
-        if (!updateDataDto.roles.includes(UserRole.user)) {
-            updateDataDto.roles.push(UserRole.user);
-        }
-
-        await this.prismaService.user.update({
-            where: {
-                id: userToUpdateId
-            },
-            data: {
-                firstName: updateDataDto.firstName,
-                lastName: updateDataDto.lastName,
-                email: updateDataDto.email,
-                passwdHash: hash,
+        } catch (error) {
+            if (error instanceof PrismaClientKnownRequestError) {
+                throw new ForbiddenException();
             }
-        });
+        }
     }
 
     public async updateUserAdmin(userToUpdateId: number, updateDataDto: UserDto) {
-        await this.checkExistsOrThrowException(userToUpdateId);
+        try {
+            const hash = await argon.hash(updateDataDto.password);
 
-        const hash = await argon.hash(updateDataDto.password);
+            updateDataDto.roles = Array.from(new Set(updateDataDto.roles));
 
-        updateDataDto.roles = Array.from(new Set(updateDataDto.roles));
-
-        if (!updateDataDto.roles.includes(UserRole.user)) {
-            updateDataDto.roles.push(UserRole.user);
-        }
-
-        await this.prismaService.user.update({
-            where: {
-                id: userToUpdateId
-            },
-            data: {
-                firstName: updateDataDto.firstName,
-                lastName: updateDataDto.lastName,
-                email: updateDataDto.email,
-                passwdHash: hash,
-                roles: updateDataDto.roles,
-                facultyId: updateDataDto.facultyId,
-                semesterId: updateDataDto.semesterId
+            if (!updateDataDto.roles.includes(UserRole.user)) {
+                updateDataDto.roles.push(UserRole.user);
             }
-        });
+
+            await this.prismaService.user.update({
+                where: {
+                    id: userToUpdateId
+                },
+                data: {
+                    firstName: updateDataDto.firstName,
+                    lastName: updateDataDto.lastName,
+                    email: updateDataDto.email,
+                    passwdHash: hash,
+                    roles: updateDataDto.roles,
+                    facultyId: updateDataDto.facultyId,
+                    semesterId: updateDataDto.semesterId
+                }
+            });
+        } catch (error) {
+            if (error instanceof PrismaClientKnownRequestError) {
+                throw new ForbiddenException();
+            }
+        }
     }
 
     public async deleteUser(userToDeleteId: number) {
-        await this.checkExistsOrThrowException(userToDeleteId);
-
-        await this.prismaService.user.delete({
-            where: {
-                id: userToDeleteId
+        try {
+            await this.prismaService.user.delete({
+                where: {
+                    id: userToDeleteId
+                }
+            });
+        } catch (error) {
+            if (error instanceof PrismaClientKnownRequestError) {
+                throw new ForbiddenException();
             }
-        });
-    }
-
-    private async checkExistsOrThrowException(userToDeleteId: number) {
-        const user = await this.prismaService.user.findFirst({
-            where: {
-                id: userToDeleteId
-            }
-        });
-
-        if (!user) {
-            throw new NotFoundException('User not found');
         }
-
-        return user
     }
 }
