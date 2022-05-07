640ad929f6d2fbe8b87dc42d51d61fce303339ac
Martin Šepeľa <xsepela@fi.muni.cz>
2022-05-07 14:48:48 +0200

    fix: Fixed guards in user controler

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,6 +1,5 @@
 import { Controller, Get, UseGuards } from '@nestjs/common';
 import { User, UserRole } from '@prisma/client';
-import { Console } from 'console';
 import { GetUser } from 'src/auth/decorator';
 import { Roles } from 'src/auth/decorator/roles.decorator';
 import { AuthenticatedGuard, RolesGuard } from 'src/auth/guard';
@@ -13,12 +12,9 @@ export class UserController {
     constructor(userservice : UserService){}
 
     @Get('me')
-    @Roles(UserRole.user)
-    @UseGuards(AuthenticatedGuard,RolesGuard)
+    @UseGuards(AuthenticatedGuard)
     public getMe(@GetUser() user: User )
     { 
-        console.log('Controller');
-        console.log(user);
-        return 'Hello there';
+        return "Approved";
     }
 }
