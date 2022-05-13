0a4705b30dec67f5c9605ac570685b08dde86d4a
xboucek <xboucek@mail.muni.cz>
2022-05-13 21:51:36 +0200

    feat: added main page component

---
+++ frontend/src/components/Pages/MainPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SubjectList from '../SubjectPreview/SubjectPreviewList';
+
+const MainPage = () => (
+  <main className="main-content">
+    <div className="main-content-container">
+      <SubjectList />
+    </div>
+  </main>
+);
+
+export default MainPage;
